dc0c9e7c091edef0ea3cb20cd10edac61d1ab1e9
Jessi Bevill <sonorousfrost@gmail.com>
2022-12-02 12:10:59 -0600

    Added capitalization function (WIP)

---
+++ script.js
@@ -0,0 +1,31 @@
+function showMessage () {
+    alert('Hello!');
+}
+// showMessage(); uncomment to activate
+
+let a = 10;
+let b = 7;
+
+function add7(a, b) {
+    return a + b;
+}
+
+console.log(add7(a, b));
+
+let c = 5;
+let d = 8;
+
+function multiply(c, d){
+    return c * d;
+}
+
+console.log(multiply(c, d))
+
+let nor = "Hello!";
+let nor2 = nor.charAt(0).toUpperCase() + nor.slice(0);
+
+let cam = "goodBye";
+let cam2 = cam.charAt(0).toUpperCase() + cam.slice(0);
+
+console.log(nor);
+console.log(cam2); // how to make Bye lowercase?
